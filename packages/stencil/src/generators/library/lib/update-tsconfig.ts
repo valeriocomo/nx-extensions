4c464562e63656a046ce2d205a87c3f1923e61ee
Valerio Como <v.como@innovazione.gov.it>
2025-03-29 17:26:08 +0000

    fix: bug on updateTsConfig

--- packages/stencil/src/generators/library/lib/update-tsconfig.ts
+++ packages/stencil/src/generators/library/lib/update-tsconfig.ts
@@ -4,6 +4,9 @@ import { Tree, updateJson } from '@nx/devkit';
 export function updateTsConfig(host: Tree, options: LibrarySchema) {
   updateJson(host, 'tsconfig.base.json', (json) => {
     const c = json.compilerOptions;
+    if(!c.paths) {
+      c.paths = {}
+    }
     delete c.paths[`${options.importPath}`];
     c.paths[`${options.importPath}`] = [`${options.projectRoot}/src/index.ts`];
     return json;
